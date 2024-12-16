3f0d602e2ac9521520de77c1f86dfa455072a445
Fern <fmenezes098@gmail.com>
2024-12-16 11:01:36 -0300

    Atualização dos nomes das pastas

---
+++ aula_08/exceptions_ts/Conversor.ts
@@ -0,0 +1,15 @@
+﻿try{
+    const resultado = converte(123);
+
+    console.log(resultado);
+}catch(error: any){
+    if(error instanceof TypeError){
+        console.error("Você digitou um número ao invés de uma string!")
+    }else{
+    console.error("Erro Inesperado!", error.message);
+    }
+}
+    export function converte(conteudo: any): string {
+        return conteudo.toUpperCase();
+    }
+
